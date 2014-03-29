4b96592a787bd52eb6c4ab021aa811530be31401
davide <d@vide.bz>
2014-03-29 15:06:22 +0100

    jar creation script

---
+++ dist/build-lib.sh
@@ -0,0 +1,20 @@
+
+LIB_VERSION="sasabus-opendata-lib-1.0."$(date '+%Y%m%d')".jar"
+
+cd $(dirname "$0")
+DIST_DIR=$(pwd -P)
+echo $DIST_DIR
+
+mkdir tmp
+
+rsync -acO ../bin/ tmp/
+rsync -acO ../src/ tmp/
+rsync -acO ../src-dmxmljson-gen/ tmp/
+
+cd tmp
+
+zip -r --filesync --no-dir-entries ../$LIB_VERSION .
+
+cd ..
+
+rm -r tmp
